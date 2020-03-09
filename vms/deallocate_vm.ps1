7bdccbb68e8e8f10d65a8212df103b5acae206ed
gavarasana <gavarasana@gmail.com>
2020-03-08 20:29:54 -0400

    delete client nodes

--- vms/deallocate_vm.ps1
+++ vms/deallocate_vm.ps1
@@ -1,5 +1,16 @@
+az login
+az account set --subscription "4e2be44e-b375-46f2-af02-be5db4e96fbf"
 # login to azure
-az login --subscription "d5f24952-8dc2-4c18-9aa8-e5ca01a92b06"
+az vm deallocate --name rgdemo-lnx-vm1 --resource-group rgdemo-rg --no-wait
+az vm deallocate --name rgdemo-lnx-vm2 --resource-group rgdemo-rg --no-wait
+az vm deallocate --name rgdemo-lnx-vm3 --resource-group rgdemo-rg --no-wait
+az vm deallocate --name rgdemo-lnx-vm4 --resource-group rgdemo-rg --no-wait
+az vm deallocate --name rgdemo-lnx-vm5 --resource-group rgdemo-rg --no-wait
+az vm deallocate --name rgdemo-lnx-vm6 --resource-group rgdemo-rg --no-wait
+az vm deallocate --name rgdemo-lnx-vm7 --resource-group rgdemo-rg --no-wait
+az vm deallocate --name rgdemo-lnx-vm8 --resource-group rgdemo-rg --no-wait
+az vm deallocate --name rgdemo-lnx-vm9 --resource-group rgdemo-rg --no-wait
+az vm deallocate --name rgdemo-lnx-vm10 --resource-group rgdemo-rg --no-wait
 
 # shutdown vm-3
 # az vm stop --name "rgdemo-linux-3" --resource-group "rgdemo-rg"

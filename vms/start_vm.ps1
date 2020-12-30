ebbbb56331bbbcebbe6a7cc2b847c2431f89ddf2
gavarasana <gavarasana@gmail.com>
2020-12-30 17:49:24 -0500

    added the resource group

--- vms/start_vm.ps1
+++ vms/start_vm.ps1
@@ -1,8 +1,11 @@
 #login to azure
-# az login --subscription "d5f24952-8dc2-4c18-9aa8-e5ca01a92b06"
+az login --subscription "d5f24952-8dc2-4c18-9aa8-e5ca01a92b06"
 
 # start the vm
-# az vm start --name "rgdemo-linux-3" --resource-group "rgdemo-rg"
+az vm start --name "rgdemo-lnx-vm1" --resource-group "rg-learn-rg" --no-wait" --resource-group "rgdemo-rg"
+
+# List IP address
+az vm list-ip-addresses --name "rgdemo-linux-1c" --resource-group "rgdemo-rg"
 
 #start the second vm
 # az vm start --name "rgdemo-linux-1c" --resource-group "rgdemo-rg"
